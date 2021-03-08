ff16d7bb0c11fc3c282b8bf253dedf490f4a9c48
lek <sxe2255@gmail.com>
2021-03-08 03:36:20 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+    if( array === undefined){
+        return 0;
+    }
+    else if(array.length === 0){
+        return 0;
+    }
+    else{
+        array.sort(function(a, b) {
+            return a - b;
+        });
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if( array === undefined){
+        return 0;
+    }
+    else if(array.length === 0){
+        return 0;
+    }
+    else{
+        array.sort(function(a, b) {
+            return a - b;
+        });
+        return array[array.length-1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if( array === undefined){
+        return 0;
+    }
+    else if(array.length === 0){
+        return 0;
+    }
+    else{
+        let avg = 0;
+        array.forEach(element => {
+            avg +=element;
+        });
+        avg = avg/array.length;
+        return avg;
+    }
 }
